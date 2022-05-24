e7d0a0590ac8348ccd8b5e847467180370412c4e
Christian Augustin <christianaugustinmusic@gmail.com>
2022-05-24 03:38:02 +0200

    "fixedsys"

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[]          = {
-	"Px437 IBM BIOS:size=7:antialias=true:autohint=true",
+	"Fixedsys Excelsior:size=12:antialias=false:autohint=true",
 	"emoji:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
