815f901924b9b5f7c04855a9597471102086104a
Christian Augustin <christianaugustinmusic@gmail.com>
2022-12-14 21:50:20 +0100

    "original font"

--- config.def.h
+++ config.def.h
@@ -4,9 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[]          = {
-
-"AcPlus IBM BIOS:size=8:antialias=true:autohint=true",
-
+"PxPlus IBM VGA 9x16:size=12:antialias=false:autohint=true",
 	"emoji:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
