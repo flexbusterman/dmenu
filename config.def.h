8ae9f3b0b047cee73cc89d18e51edb75ebafc7eb
Christian Augustin <johnchristianaugustin@gmail.com>
2026-03-17 20:13:03 +0100

    uniform background color

--- config.def.h
+++ config.def.h
@@ -22,7 +22,8 @@ static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
     // [SchemeNorm] = { "#f8f8f2", "#282a36" },
     [SchemeNorm] = {"#f8f8f2", "#000000"},
-    [SchemeSel] = {"#f8f8f2", "#6272a4"},
+    // [SchemeSel] = {"#f8f8f2", "#6272a4"},
+    [SchemeSel] = {"#f8f8f2", "#0000fa"},
     [SchemeOut] = {"#000000", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
