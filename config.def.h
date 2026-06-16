9b968cf45a3438345c5585f4b3ee276bda9393f5
Christian Augustin <johnchristianaugustin@gmail.com>
2026-06-16 21:51:06 +0200

    Green theme for VM

--- config.def.h
+++ config.def.h
@@ -23,7 +23,7 @@ static const char *colors[SchemeLast][2] = {
     // [SchemeNorm] = { "#f8f8f2", "#282a36" },
     [SchemeNorm] = {"#f8f8f2", "#000000"},
     // [SchemeSel] = {"#f8f8f2", "#6272a4"},
-    [SchemeSel] = {"#f8f8f2", "#005577"},
+    [SchemeSel] = {"#f8f8f2", "#005f00"},
     // [SchemeSel] = {"#f8f8f2", "#0000fa"},
     [SchemeOut] = {"#000000", "#00ffff"},
 };
